d409b966ad33f7f930fd017f0cbfaacfa9a2bf92
Vusal004 <vusalveliyev652a3@gmail.com>
2024-09-17 22:56:58 +0400

    translate part-2

--- src/components/SubscriptionsPage.tsx
+++ src/components/SubscriptionsPage.tsx
@@ -27,7 +27,7 @@ const SubscriptionsPage = () => {
 
   return (
     <div className="w-full">
-      <h1 className="text-center text-2xl font-bold mb-8">{t("choose_subscription")}</h1>
+      <h1 className="text-center text-2xl font-bold mb-8">{t("title")}</h1>
       <div className="w-full flex flex-col gap-4">
         {subscriptions.map((subscription, index) => (
           <SubscriptionCard
