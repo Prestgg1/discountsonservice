8a252f4ef7271c51cb46c502fcf0b915bfd42aad
Prestgg1 <prestgg12@gmail.com>
2024-10-02 07:08:48 +0400

    Subscription Duration Fixed

---
+++ src/app/api/checkout/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!,{
+  typescript: true
+});
+
+export async function POST(req: NextRequest) {
+  const cart = await req.json();
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: cart.name,
+            },
+            unit_amount: cart.price * 100,
+          },
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      cancel_url: `http://localhost:3000/?canceled=true`,
+      success_url: `http://localhost:3000?success=true&session_id={CHECKOUT_SESSION_ID}`,
+    });
+    //👇🏻 return the session URL
+    return NextResponse.json({ session: session.url });
+  } catch (err) {
+    return NextResponse.json({ error: err }, { status: 500 });
+  }
+}
--- src/components/subscriptions/Singlesubs.tsx
+++ src/components/subscriptions/Singlesubs.tsx
@@ -10,12 +10,20 @@ export default function Singlesubs({ sub, durations }: { sub: { name: string,des
     <>
       <div className='flex flex-col gap-2 2xl:gap-5 w-full justify-center items-center '>
         <h1 className='md:text-5xl text-3xl font-bold tracking-wide'> {t('description', { subname: sub.name })} </h1>
-        <p className='mt-3 md:text-xl'>{sub.description[locale]} </p>
+        <p className='mt-3 md:text-2xl'>{sub.description[locale]} </p>
 
           <div className="join">
-          {durations.map((duration: { duration: number },index:number) => (
+          {durations.length > 1 ? durations.map((duration: { duration: number },index:number) => (
              <input key={index} className="join-item      btn" onClick={() => setSelectedduration(duration.duration)} checked={selectedduration === duration.duration} type="radio" name="options" aria-label={`${duration.duration} ${t('mounth')}`} />
-          ))}
+          )) :    <span
+          className="text-xl"
+          dangerouslySetInnerHTML={{
+            __html: t('select', {
+              duration: `<span class="text-blue-600">${durations[0].duration} ${t('mounth')}</span>`
+            })
+          }}
+        />}
+    <span className="3xl "></span>
           </div>
       </div>
 
