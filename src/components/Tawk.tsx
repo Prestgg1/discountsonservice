e15930bfa97765fa83283e8a5c550271d6e68e87
Prestgg1 <prestgg12@gmail.com>
2024-10-06 18:10:03 +0400

    Crisp to added

--- src/components/Tawk.tsx
+++ src/components/Tawk.tsx
@@ -2,26 +2,24 @@ import Script from 'next/script';
 
 export default function Tawk() {
   return (
-    <div>
-      {/* Tawk.to Script */}
-      
+    <>
       <Script
-        id="tawk-to-script"
-        strategy="afterInteractive" // Sayfa yüklendikten sonra script çalıştırılır
+        id="crisp-chat"
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
-            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src='https://embed.tawk.to/67028af4256fb1049b1e1382/1i9gutipc';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            s0.parentNode.insertBefore(s1,s0);
+            window.$crisp=[]; 
+            window.CRISP_WEBSITE_ID="f86307f0-339a-4ee9-869a-f8837647d2ef";
+            (function() {
+              var d = document;
+              var s = d.createElement("script");
+              s.src = "https://client.crisp.chat/l.js";
+              s.async = 1;
+              d.getElementsByTagName("head")[0].appendChild(s);
             })();
           `,
         }}
       />
-    </div>
+    </>
   );
 }
