5c34be986e4be6cc86b6908fb18b8984ce2c3092
Prestgg1 <prestgg12@gmail.com>
2024-11-01 23:09:54 +0400

    Reset Password

--- src/app/api/change-password/route.js
+++ src/app/api/change-password/route.js
@@ -1,21 +1,13 @@
 import { NextResponse } from 'next/server';
-import { NextRequest } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcryptjs';
 import { passwordValidationSchema } from '@/app/schema/resetpassword';
 
 const prisma = new PrismaClient();
-
-interface ResetPasswordRequest {
-  email: string;
-  verificationCode: string;
-  newPassword: string;
-  confirmPassword: string;
-}
-
-export async function POST(req: NextRequest) {
+const newDate = new Date()
+export async function POST() {
   try {
-    const { email, verificationCode, newPassword, confirmPassword }: ResetPasswordRequest = await req.json();
+    const { email, verificationCode, newPassword, confirmPassword } = await req.json();
 
     await passwordValidationSchema.validate({ newPassword, confirmPassword });
 
@@ -27,7 +19,7 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ message: 'İstifadəçi Tapılmadı' }, { status: 404 });
     }
 
-    if (user.verificationCode !== verificationCode || !user.verificationCodeExpiry || user.verificationCodeExpiry < new Date()) {
+    if (user.verificationCode !== verificationCode || !user.verificationCodeExpiry || user.verificationCodeExpiry < newDate) {
       return NextResponse.json({ message: 'Ya Səhv Kod Yazırsan Yada Surokun Keçib' }, { status: 400 });
     }
 
@@ -43,9 +35,9 @@ export async function POST(req: NextRequest) {
     });
 
     return NextResponse.json({ message: 'Şifrə Dəyişdirildi' }, { status: 200 });
-  } catch (error: unknown) {
+  } catch (error) {
   
-    if (error instanceof Error) {
+    if (error) {
       return NextResponse.json({ message: error.message || 'Bir hata oluştu' }, { status: 500 });
     }
     return NextResponse.json({ message: 'Səbuhi serverdə problem var' }, { status: 500 });
