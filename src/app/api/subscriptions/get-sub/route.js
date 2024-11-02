d6a5f2aab57871fc6c01aa5991d75ac3b90fd45c
Prestgg1 <prestgg12@gmail.com>
2024-11-02 15:54:12 +0400

    Request Elave edildi

--- src/app/api/subscriptions/get-sub/route.js
+++ src/app/api/subscriptions/get-sub/route.js
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export async function GET() {
+export async function GET(req) {
   const slug = req.nextUrl.searchParams.get('slug');
 
   if (!slug) {
